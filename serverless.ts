65ba6700f6cccea7855f69ac6761496355521afb
samad <samadarshad@hotmail.co.uk>
2021-04-24 19:29:38 +0100

    Initial Deploy

---
+++ serverless.ts
@@ -0,0 +1,36 @@
+import hello from '@functions/hello';
+import type { AWS } from '@serverless/typescript';
+
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless-capstone',
+  app: 'serverless-capstone',
+  org: 'samadarshad',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    // @ts-ignore
+    region: "${opt:region, 'eu-west-2'}",
+    stage: "${opt:stage, 'dev'}",
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
